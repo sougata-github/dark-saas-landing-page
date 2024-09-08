a1ddf8875eb23fec70a67c4d475a034ef55dd374
Sougata <sougatadas9874@gmail.com>
2024-09-08 19:02:12 +0530

    add logo ticker animation.

--- components/LogoTicker.tsx
+++ components/LogoTicker.tsx
@@ -1,11 +1,15 @@
+"use client";
+
 import Image from "next/image";
 
-import acme from "@/assets/images/acme.png";
-import quantum from "@/assets/images/quantum.png";
-import echo from "@/assets/images/echo.png";
-import celestial from "@/assets/images/celestial.png";
-import pulse from "@/assets/images/pulse.png";
+import { motion } from "framer-motion";
+
 import apex from "@/assets/images/apex.png";
+import acme from "@/assets/images/acme.png";
+import echo from "@/assets/images/echo.png";
+import pulse from "@/assets/images/pulse.png";
+import quantum from "@/assets/images/quantum.png";
+import celestial from "@/assets/images/celestial.png";
 
 const images = [acme, quantum, echo, celestial, pulse, apex];
 
@@ -16,22 +20,39 @@ const LogoTicker = () => {
         <h2 className="text-xl text-white/70">
           Trusted by the world&apos;s most innovative teams.
         </h2>
-        <div className="overflow-hidden mt-9 before:content-[''] after:content-[''] before:absolute after:absolute before:h-full after:h-full before:w-5 after:w-20 after:right-0 before:left-0 before:top-0 after:top-0 before:bg-[linear-gradient(to_right,#000,rgba(0,0,0,0))] after:bg-[linear-gradient(to_left,#000,rgba(0,0,0,0))] relative">
-          <div className="flex gap-16">
-            {images.map((image) => (
-              <Image
-                priority
-                unoptimized
-                quality={100}
-                src={image.src}
-                alt={`${image}-logo`}
-                key={image.src}
-                height={50}
-                width={50}
-                className="flex-none h-8 w-auto"
-              />
+        <div className="flex overflow-hidden mt-9 before:content-[''] after:content-[''] before:absolute after:absolute before:h-full after:h-full before:w-5 after:w-20 after:right-0 before:left-0 before:top-0 after:top-0 before:bg-[linear-gradient(to_right,#000,rgba(0,0,0,0))] after:bg-[linear-gradient(to_left,#000,rgba(0,0,0,0))] before:z-10 relative">
+          <motion.div
+            initial={{
+              translateX: 0,
+            }}
+            animate={{
+              translateX: "-50%",
+            }}
+            transition={{
+              duration: 10,
+              ease: "linear",
+              repeat: Infinity,
+            }}
+            className="flex flex-none gap-16 pr-16"
+          >
+            {[...new Array(2)].fill(0).map((_, idx) => (
+              <>
+                {images.map((image) => (
+                  <Image
+                    priority
+                    unoptimized
+                    quality={100}
+                    src={image.src}
+                    alt={`${image}-logo`}
+                    key={image.src}
+                    height={50}
+                    width={50}
+                    className="flex-none h-8 w-auto"
+                  />
+                ))}
+              </>
             ))}
-          </div>
+          </motion.div>
         </div>
       </div>
     </div>
